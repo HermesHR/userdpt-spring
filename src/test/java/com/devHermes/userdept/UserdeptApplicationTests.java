bc9057f277d06c51e582603cd196f73e2243ea46
HermesHR <filhos.hr@gmail.com>
2022-09-30 10:58:14 -0300

    first commit

---
+++ src/test/java/com/devHermes/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devHermes.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
